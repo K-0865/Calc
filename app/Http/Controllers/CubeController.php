e0edc951c4314c458d0083719c3f877758e514fd
ryouhei-kanezawa <k.ryouhei1414@gmail.com>
2022-02-16 14:05:36 +0900

    server

--- app/Http/Controllers/CubeController.php
+++ app/Http/Controllers/CubeController.php
@@ -13,10 +13,9 @@ class CubeController extends Controller
 
         $cube = $in * $in *$in;
         $name = "Cube";
-        //$link = mysqli_connect('localhost', 'root', '', 'network');
-        //$stmt = mysqli_prepare($link, "INSERT INTO results (name, result) VALUES (?, ?)");
-        $link = mysqli_connect('mysql57.tcagame01.sakura.ne.jp', 'tcagame01', 'tcagame2021', 'tcagame01_20j70012');
-        $stmt = mysqli_prepare($link, "INSERT INTO calculation_results (name, result) VALUES (?, ?)");
+        $link = mysqli_connect('localhost', 'root', '', 'network');
+        $stmt = mysqli_prepare($link, "INSERT INTO results (name, result) VALUES (?, ?)");
+       
         mysqli_stmt_bind_param($stmt, "ss", $name, $cube);
         $result = mysqli_stmt_execute($stmt);
         
