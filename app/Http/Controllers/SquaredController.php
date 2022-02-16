6c3df73c0f41fb6ef2651330481f0b849612f718
ryouhei-kanezawa <k.ryouhei1414@gmail.com>
2022-02-16 14:04:40 +0900

    server

--- app/Http/Controllers/SquaredController.php
+++ app/Http/Controllers/SquaredController.php
@@ -13,8 +13,10 @@ class SquaredController extends Controller
         if(is_numeric($in)){
 
         $result_in = $in * $in;
-        $link = mysqli_connect('localhost', 'root', '', 'network');
-        $stmt = mysqli_prepare($link, "INSERT INTO results (name, result) VALUES (?, ?)");
+        //$link = mysqli_connect('localhost', 'root', '', 'network');
+        //$stmt = mysqli_prepare($link, "INSERT INTO results (name, result) VALUES (?, ?)");
+        $link = mysqli_connect('mysql57.tcagame01.sakura.ne.jp', 'tcagame01', 'tcagame2021', 'tcagame01_20j70012');
+        $stmt = mysqli_prepare($link, "INSERT INTO calculation_results (name, result) VALUES (?, ?)");
         mysqli_stmt_bind_param($stmt, "ss", $name, $result_in);
         $result = mysqli_stmt_execute($stmt);
         
